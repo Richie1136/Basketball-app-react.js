58e126b04b076085ae331a9308c979fc925dfec8
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-12-29 11:39:48 -0500

    Removed commented out code

--- basketball-app/src/components/routes.js
+++ basketball-app/src/components/routes.js
@@ -5,7 +5,6 @@ import { Routes, Route } from 'react-router-dom'
 import Navigation from "./navigation/Navigation";
 import Standings from "./standings/Standings";
 
-
 const AllRoutes = () => (
   <>
     <Navigation />
@@ -14,7 +13,6 @@ const AllRoutes = () => (
       <Route path='/:team' element={<TeamRoster />} />
       <Route path="/player/:playerid" element={<Player />} />
       <Route path='/standings' element={<Standings />} />
-      {/* <Route path="/standings/:Key" element={<TeamRoster />} /> */}
     </Routes>
   </>
 )
